ce0ba692cda6657fdb7cdeee7bc57e7c2083b9b2
MA024445 <mary.anderson@cerner.com>
2017-01-19 16:12:54 -0600

    Added random functionality, & tomorrow's movie

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
-
+        oscarMovies = ["La La Land", "Nocturnal Animals", "Jackie", "Arrival", "The Blind Christ"]
         # TODO: randomly choose one of the movies, and return it
 
-        return "The Big Lebowski"
+        return random.choice(oscarMovies)
 
     def get(self):
         # choose a movie by invoking our new function
@@ -16,6 +17,12 @@ class Index(webapp2.RequestHandler):
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
+        content += "<p>" + movie + "</p>"
+
+        # choose another movie
+        movie = self.getRandomMovie()
+
+        content += "<h1>Tomorrow's Movie</h1>"
         content += "<p>" + movie + "</p>"
 
         # TODO: pick a different random movie, and display it under
