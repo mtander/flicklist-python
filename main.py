34d4caefd180b5fd85fd002ea5199d26e6fa3ca3
MA024445 <mary.anderson@cerner.com>
2017-01-29 17:57:13 -0600

    Added studio1 code

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
+    def getRandomMovie(self):
+        myList = [
+        "Jurassic Park", "American Beauty", "The Curious Case of Benjamin Button",
+        "Star Wars", "Aladdin"
+        ]
+        randint = random.randint(0,len(myList)-1)
+        movie = myList[randint]
+        return movie     
     def get(self):
         #self.response.write('Hello, Summer of Code!')
         
         movie = self.getRandomMovie()
         
-        content  = "<h1>Movie of the Day</h1>"
+        content = "<h1>My Favorite Movie</h1>"
         content += "<p>" + movie + "</p>"
         
         self.response.write(content)
-        
-    def getRandomMovie(self):
-        #return a hardcoded string of a movie title
-        return "The Big Lebowski"
     
 app = webapp2.WSGIApplication([
     ('/', Index)
