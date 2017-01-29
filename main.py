cc7a2dc76b4186ea18d443caf3e0577555ed0994
MA024445 <mary.anderson@cerner.com>
2017-01-29 16:27:58 -0600

    Adding walkthrough1: created getRandomMovie, used to build content variable, then self.response.write(content)

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import webapp2
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, Summer of Code!')
-
+        #self.response.write('Hello, Summer of Code!')
+        
+        movie = self.getRandomMovie()
+        
+        content  = "<h1>Movie of the Day</h1>"
+        content += "<p>" + movie + "</p>"
+        
+        self.response.write(content)
+        
+    def getRandomMovie(self):
+        #return a hardcoded string of a movie title
+        return "The Big Lebowski"
+    
 app = webapp2.WSGIApplication([
     ('/', Index)
 ], debug=True)
