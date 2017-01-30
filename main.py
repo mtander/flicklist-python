8554937727ec3b59893537b4b88d8dceb524e43f
MA024445 <mary.anderson@cerner.com>
2017-01-29 20:37:40 -0600

    Added work for walkthrough3 & studio3

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import webapp2
+# https://cloud.google.com/appengine/docs/python/getting-started/handling-user-input-form
+        
+        #validation for add--check 3 things:
+        #   1) includes html tags.  use cgi.escape
+        #   2) no movie included.  Display "Please specify the name of the movie you want to add"
+        #        , also reject form submission and redirect back to home page
+        #   3) trying to add a movie that sucks. Redirect to homepage, display
+        #       "Trust me, you don't want to add 'moviename' to your Watchlist"
 
+import webapp2
+import cgi
+
+def isBlank (myString):
+    if myString and myString.strip():
+        #myString is not None AND myString is not empty or blank
+        return False
+    #myString is None OR myString is empty or blank
+    return True
 
 # html boilerplate for the top of every page
 page_header = """
@@ -7,9 +23,16 @@ page_header = """
 <html>
 <head>
     <title>FlickList</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-    <h1>FlickList</h1>
+    <h1>
+        <a href="/">FlickList</a>
+    </h1>
 """
 
 # html boilerplate for the bottom of every page
@@ -18,6 +41,18 @@ page_footer = """
 </html>
 """
 
+terrible_movies = ['Texas Chainsaw Massacre','Paul Blart: Mall Cop', 
+            'The Legend of Tarzan', 'The Last Airbender', 'Requiem for a Dream']
+
+def getCurrentWatchlist():
+    """ Returns the user's current watchlist """
+    myList = [
+        "Jurassic Park", "American Beauty", "The Curious Case of Benjamin Button",
+        "Star Wars", "Aladdin"
+        ]
+    #won't be able to save this until we work on databases.  For now, just return hardcoded list.
+    return myList
+    
 class Index(webapp2.RequestHandler):
     """ Handles requests coming in to '/' (the root of our site)
         e.g. www.flicklist.com/
@@ -38,25 +73,40 @@ class Index(webapp2.RequestHandler):
             <input type="submit" value="Add It"/>
         </form>
         """
-
+  
+        #build dropdown for crossoff_form
+        # the "{0}" is an indicator to the format method that you want it replaced
+        # by the first (index 0) parameter of format
+        # http://stackoverflow.com/questions/6682806/what-does-0-mean-in-this-python-string
+        crossoff_options = ""
+        for movie in getCurrentWatchlist():
+            crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
+        
         # a form for crossing off movies
         crossoff_form = """
         <form action="/cross-off" method="post">
             <label>
                 I want to cross off
                 <select name="crossed-off-movie"/>
-                    <option value="Star Wars">Star Wars</option>
-                    <option value="Minions">Minions</option>
-                    <option value="Freaky Friday">Freaky Friday</option>
-                    <option value="My Favorite Martian">My Favorite Martian</option>
+                    {0}
                 </select>
                 from my watchlist.
             </label>
             <input type="submit" value="Cross It Off"/>
         </form>
-        """
+        """.format(crossoff_options)
+        
+        # if error, display it
+        # even though we build error, still escape it w/ cgi so a malicious user
+        # can't pass in something that will break the html
+        error = self.request.get("error") # Gets error from query parameter (/?error="blahblah")
+        if error:
+            error_esc = cgi.escape(error, quote=True)
+            error_element = "<p class='error'>" + error_esc + "</p>"
+        else:
+            error_element = ''
 
-        page_content = edit_header + add_form + crossoff_form
+        page_content = edit_header + add_form + crossoff_form + error_element
         content = page_header + page_content + page_footer
         self.response.write(content)
 
@@ -66,10 +116,21 @@ class AddMovie(webapp2.RequestHandler):
         e.g. www.flicklist.com/add
     """
 
-    def post(self):
+    def post(self):   
         # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
-
+        new_movie = cgi.escape(new_movie, quote=True)
+        
+        #if user typed nothing, redirect & yell at them
+        if isBlank(new_movie):
+            #error
+            error = "Please specify the name of the movie you want to add"
+            #redirect
+            self.redirect("/?error=" + error)
+        elif new_movie in terrible_movies:
+            error = "Trust me, you don't want to add {} to your Watchlist".format(new_movie)
+            self.redirect("/?error=" + error)
+            
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
         sentence = new_movie_element + " has been added to your Watchlist!"
@@ -88,7 +149,17 @@ class CrossOffMovie(webapp2.RequestHandler):
         # look inside the request to figure out what the user typed
         crossed_off_movie = self.request.get("crossed-off-movie")
 
-        # build response content
+        if(crossed_off_movie in getCurrentWatchlist()) == False:
+            #User tried to enter movie that isn't in list, so redirect them
+            #back to front page and display error
+            
+            #create error message
+            error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
+        
+            #redirect to homepage, and include error as query parameter in URL
+            self.redirect("/?error=" + error)
+        
+        # build response content (will not reach this point if already redirected)
         crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
         confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
 
